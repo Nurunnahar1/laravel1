2897e4121f73f98bdfcb94410048863367b2eaeb
Nurunnahar <nurunnahar9081@gmail.com>
2023-01-29 23:56:08 +0600

    change code

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 1. the Feasibility Study of the SDLC model is:
     Feasibility study is an important step in the Software Development Life Cycle.
 
@@ -22,3 +31,5 @@
     .Testing
     .Deployment
     .Maintenance
+</body>
+</html>
