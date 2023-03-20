012f0154583d0184af933929265c4b92b89980e7
Nurunnahar <nurunnahar9081@gmail.com>
2023-03-20 19:44:22 +0600

    module 7 assignment

---
+++ modulee 7 assignment-database.sql
@@ -0,0 +1,86 @@
+CREATE TABLE `departments` (
+  `id` int(20) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `manager` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `departments`
+--
+
+INSERT INTO `departments` (`id`, `name`, `manager`) VALUES
+(20, 'Graphics', 'Mahi'),
+(21, 'Marketing', 'Sazzad'),
+(22, 'Marketing', 'Tuhin'),
+(23, 'Finance', 'Sakib'),
+(24, 'Human Resources', 'Siam'),
+(25, 'Human Resources', 'Saddam'),
+(26, 'Research', 'John Doe');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `id` int(20) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `age` int(20) DEFAULT NULL,
+  `salary` int(20) DEFAULT NULL,
+  `department_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`id`, `name`, `age`, `salary`, `department_id`) VALUES
+(1001, 'Jibon Ahmed Jan', 23, 60000, 20),
+(1002, 'Mamun', 22, 30000, 21),
+(2003, 'Shahriar', 25, 75000, 22),
+(2004, 'Mehedi Hassan', 27, 55000, 23),
+(2005, 'Toufiq Hassan', 32, 80000, 24),
+(2006, 'Saddam', 28, 60000, 25);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `department_id` (`department_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `departments`
+--
+ALTER TABLE `departments`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `employees`
+--
+ALTER TABLE `employees`
+  ADD CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
